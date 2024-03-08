48a10f94e9ce1a98066364b18b8823d0399d4373
Ozuba <miguelozubas@gmail.com>
2024-03-08 02:03:17 +0100

    Finalized Functioning prototype

--- modem.m
+++ modem.m
@@ -7,15 +7,15 @@ N = 200; % Number of symbols
 
 t = 0:1/fs:T; % Period
 
+%%-------------------------------------- base Ortonormal --------------------------------------%%
 
-avFreqs = [8e3 22.050e3 44.1e3];
 %% Definición de los paramtetros del Altavoz/Micro
 
 tRange = [] %Transceiver Dynamic Range
 rRange = [] %Receiver Dynamic Range
 % Elegimos el rango optimo
-fRange(1) = 400;
-fRange(2) = 8e3;
+fMin = 400;
+fMax = 8e3;
 
 
 %{
@@ -23,18 +23,73 @@ Vamos a generar una base ortonormal de N vectores dentro del rango dinamico
 compartido por emisor/receptor, el espaciado vendrá restringido por la frecuencia fudamental
 que elegiremos en función del ancho de banda
 %}
-%Eleccion de la frecuencia fundamental
-ff =
-
+%Eleccion de la frecuencia fundamental (Espaciado)
+range = fMax-fMin;
+%Las queremos lo más separadas posible en el rango
+ff = range/N
+%Generamos el vector de frecuencias armonicas
+fArm = ff*(0:N-1) + fMin;
 
 %Generamos la matriz base
 base = zeros(N,length(t));
 
-
-for k = 1:N
-  base(k,:) = sin(2*pi*ff*t);
+%Generacion de la base ortogonal y normalizacion
+for i = 1:N
+  base(i,:) = sin(2*pi*fArm(i)*t);
+  l2norm = sqrt(trapz(1/fs,abs(base(i,:)).^2));
+  base(i,:) = base(i,:)/l2norm; % Normalizamos con su norma l2
 end
 
-%%
+
+
+for i = 1:N
+  sound(base(i,1:10000),fs)
+  pause(0.1)
+end
+
+%%-------------------------------------- Transmision --------------------------------------%%
+
+% Generacion del Mensaje
+text = ' Hello world '
+
+bitstream = str2num(reshape(dec2bin(uint8(text),8).', 1, [])')';
+%Cambiamos los 0 por -1 para modular
+mod = bitstream;
+mod(mod == 0) = -1
+%signal to be sent
+sig = zeros(1,length(t));
+
+for k = 1:length(mod)
+  sig = sig + base(k,:)*mod(k);
+end
+
+%sound(sig,fs)
+
+
+%%-------------------------------------- Demodulación --------------------------------------%%
+
+demod = zeros(1,N);
+
+for k = 1:N
+  demod(k) = trapz(1/fs,base(k,:).*sig);
+end
+
+%%Limpiamos morralla y recortamos al tamaño del envio
+%demod(abs(demod) < 1e-12 ) = 0;
+demod = int32(demod(1:length(mod)))
+% Reconstruimos nuestros 0
+demod(demod == -1) = 0;
+
+% Volvemos al texto
+
+recText = char(bin2dec(reshape(char(demod + '0'),8,[]).'))'
+
+
+
+
+
+
+
+
 
 
